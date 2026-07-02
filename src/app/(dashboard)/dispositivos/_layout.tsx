4138da583f515a4d5c72e4a866c5a06542248640
Rafael Rocha <rafaelgus3000@gmail.com>
2026-07-02 13:42:25 -0300

    feat: adiciona a tela de cadastro no layout

--- src/app/(dashboard)/dispositivos/_layout.tsx
+++ src/app/(dashboard)/dispositivos/_layout.tsx
@@ -1,12 +1,20 @@
 import { Stack } from 'expo-router';
 
-export default function AmbientesLayout() {
+export default function DispositivosLayout() {
   return (
     <Stack>
       {/* index é a listagem, o ponto de entrada da aba */}
       <Stack.Screen 
         name="index" 
         options={{ title: 'Meus Dispositivos' }}  
+      />
+
+      <Stack.Screen 
+        name="cadastro" 
+        options={{ 
+          title: '',
+          headerBackTitle: 'Voltar' 
+        }} 
       />
     </Stack>
   );
