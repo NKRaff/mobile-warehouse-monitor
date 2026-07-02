e24120eb5e077a4b0b8f26cb716f5010f1c88c93
Rafael Rocha <rafaelgus3000@gmail.com>
2026-07-02 11:16:11 -0300

    feat: atualiza login para salvar o id do usuario em um context

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,9 +1,12 @@
+import { AuthProvider } from '@/src/contexts/AuthContext';
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </AuthProvider>
   );
 }
